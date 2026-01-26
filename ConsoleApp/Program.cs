6346cc856de9052348614616e56ba44f1d53ce81
ageof <ageof@WINEMERALD25>
2026-01-26 13:15:58 +0100

    fixed typo

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,14 +17,12 @@ static void DoNumbersStuff()
     var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
     var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-    //here using my GetNumberString function to convert int to string
     for (int i = 0; i < numbers.Length; i++)
     {
         string numberString = GetNumberString(numbers[i]);
         Console.WriteLine($"{numbers[i]} -> {numberString}");
     }
 
-    // Overload for int input to string output
     static string GetNumberString(int number)
     {
         var numbersDictionaryIntToString = new Dictionary<int, string>
